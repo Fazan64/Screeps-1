30b5ebbccbab1c868e082146225f3275e6d9c5be
Max <max.maisak@gmail.com>
2015-08-24 16:16:23 +0300

    Refactor miner

--- gamefiles/roles_miner.js
+++ gamefiles/roles_miner.js
@@ -55,9 +55,11 @@ var miner =
 		var creepsNeeded = Math.round((steps * 8) / 100);
 
 		if(creepsNeeded > 5)
+		{
 			creepsNeeded = 5;
+		}
 
-		for(var i = 0; i < creepsNeeded; i++)
+		for (var i = 0; i < creepsNeeded; i++)
 		{
 			source.room.memory.needs.creeps.unshift (
 			{ 
@@ -80,6 +82,7 @@ var miner =
 		creep.memory.helpers = [];
 
 		var source = this.getOpenSource();
+		creep.say ("Source found!");
 		this.setSourceToMine (source);
 
 		creep.memory.onCreated = true;
@@ -89,33 +92,35 @@ var miner =
 	{
 		var creep = this.creep;
 
-		//Basically, each miner can empty a whole source by themselves. Also, since they're slow, we don't have them
-		//moving away from the source when it's empty, it'd regenerate before they got to another one.
-		//For this, we assign one miner to one source, and they stay with it
+		// Basically, each miner can empty a whole source by themselves. Also, since they're slow, we don't have them
+		// moving away from the source when it's empty, it'd regenerate before they got to another one.
+		// For this, we assign one miner to one source, and they stay with it
 		var source = Game.getObjectById (creep.memory.source);
 
 		if (source == null) 
 		{
+			creep.say ("I have no source assigned, searching...");
 			var source = this.getOpenSource();
 
-			if(!source)
+			if (!source)
+			{
+				creep.say ("No open sources!");
 				return;
-
-			this.setSourceToMine(source);
+			}
+			
+			creep.say ("Source found!");
+			this.setSourceToMine (source);
 		}
 
-		if(creep.pos.inRangeTo(source, 5))
-			creep.memory.isNearSource = true;
-		else
-			creep.memory.isNearSource = false;
+		creep.memory.isNearSource = creep.pos.inRangeTo (source, 5);
 
-		if(Memory.sources[source.id] == undefined)
-			Memory.sources[source.id] = { id: source.id };
-
+		if (Memory.sources [source.id] == undefined)
+		{
+			Memory.sources [source.id] = { id: source.id };
+		}
 		Memory.sources[source.id].miner = creep.id;
 
-		creep.moveTo(source);
-		creep.harvest(source);
+		this.moveAndPerform (source, creep.harvest);
 	}
 };
 
