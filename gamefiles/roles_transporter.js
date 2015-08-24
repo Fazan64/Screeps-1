a48673cfe4029cad6a736128e7863e7cdb1aa1c3
Max <max.maisak@gmail.com>
2015-08-24 18:06:11 +0300

    Refactor transporter

--- gamefiles/roles_transporter.js
+++ gamefiles/roles_transporter.js
@@ -1,3 +1,6 @@
+/**
+ * These transport energy from spawns to builders and extensions (in that order)
+ */
 var transporter = {
 	
 	baseParts : [CARRY, MOVE],
@@ -8,57 +11,67 @@ var transporter = {
 
 		//@TODO: Balance Spawns here
 
-		if(creep.energy == 0)
+		if (creep.energy == 0)
 		{
-			var closestSpawn = creep.pos.findNearest(Game.MY_SPAWNS, {
+			var closestSpawn = this.getClosest (Game.spawns, {
 				filter: function(spawn)
 				{
 					return spawn.energy > 0;
 				}
 			});
 
-			creep.moveTo(closestSpawn);
-			closestSpawn.transferEnergy(creep);
+			creep.moveTo (closestSpawn);
+			closestSpawn.transferEnergy (creep);
 
 			return;
 		}
 
 		var target = null;
 
-		//Transfer to builder
-		if (!target) {
-			var builderToHelp = creep.pos.findNearest(Game.MY_CREEPS, {
-				filter: function (builder) {
+		// Transfer to builder
+		if (!target) 
+		{
+			var builderToHelp = this.getClosest (Game.creeps, {
+				filter: function (builder) 
+				{
 					return builder.memory.role == "builder"
-						&& builder.energy < ( builder.energyCapacity - 10);
+						&& builder.energy < builder.energyCapacity - 10;
 				}
 			});
 
 			if (builderToHelp)
+			{
 				target = builderToHelp;
+			}
 		}
 
-		if(!target)
+		// Transfer to extensions
+		if (!target)
 		{
-			var extension = creep.pos.findNearest(Game.MY_STRUCTURES, {
-				filter: function(structure)
+			var extension = this.getClosest (Game.structures, {
+				filter: function (structure)
 				{
-					return structure.structureType == Game.STRUCTURE_EXTENSION &&
+					return structure.structureType == STRUCTURE_EXTENSION &&
 						structure.energy < structure.energyCapacity;
 				}
 			});
 
-			if(extension)
+			if (extension)
+			{
 				target = extension;
+			}
 		}
 
 		//Go to target and give it energy
-		if (creep.pos.isNearTo(target)) {
-			if (target.energy < target.energyCapacity) {
+		if (creep.pos.isNearTo(target))
+		{
+			if (target.energy < target.energyCapacity) 
+			{
 				creep.transferEnergy(target);
 			}
 		}
-		else {
+		else 
+		{
 			creep.moveTo(target);
 		}
 	}
