e76b6f29174299738e85034c115f3d74e7a9a6fc
Max <max.maisak@gmail.com>
2015-08-30 13:00:26 +0300

    minor

--- gamefiles/calculateCost.js
+++ gamefiles/calculateCost.js
@@ -1,11 +1,11 @@
 var COST            = {};
-COST[MOVE]          = 50;
-COST[WORK]          = 100;
-COST[CARRY]         = 50;
-COST[ATTACK]        = 80;
-COST[RANGED_ATTACK] = 150;
-COST[HEAL]          = 250;
-COST[TOUGH]         = 10;
+COST [MOVE]          = 50;
+COST [WORK]          = 100;
+COST [CARRY]         = 50;
+COST [ATTACK]        = 80;
+COST [RANGED_ATTACK] = 150;
+COST [HEAL]          = 250;
+COST [TOUGH]         = 10;
 
 /**
  * Calculates the energy cost of a Creep with bodyparts 'parts'
