14a4b69ea6b15b7d5e17f9653433ff0ecae8978b
Max <max.maisak@gmail.com>
2015-09-05 17:58:24 +0300

    change code structure

--- gamefiles/main.js
+++ gamefiles/main.js
@@ -3,54 +3,58 @@ var PROFILER_REPORT_INTERVAL = 20;
 
 var profiler = require ('profiler');
 
+function act ()
+{
+	var performRoles = require ('performRoles');
+	var spawner = require ('spawner');
+	var roomManager = require ('roomManager');
+	var ProtoRole = require ('role_prototype');
+	
+	if (ENABLE_PROFILING)
+	{
+		profiler.wrap (RoomPosition.prototype, 'findPathTo');
+		profiler.wrap (RoomPosition.prototype, 'findClosest');
+		
+		profiler.wrap (Game, 'getObjectById');
+		
+		profiler.wrap (roomManager, 'updateNeeds');
+		
+		profiler.wrap (ProtoRole.prototype, 'reset');
+	}
+	
+	for (var i in Game.rooms)
+	{
+		roomManager.reset ();
+		roomManager.setRoom (Game.rooms [i]);
+		
+		roomManager.initMemory ();
+		roomManager.updateNeeds ();
+	}
+	
+	for (var i in Game.spawns)
+	{
+		spawner (Game.spawns [i]);
+	}
+	
+	performRoles (Game.creeps);
+	
+	if (Game.time % PROFILER_REPORT_INTERVAL == 0)
+	{
+		Memory.profilerDump = Memory.profilerDump || {};
+		
+		var report = profiler.report ();
+		
+		profiler.logReport (report);
+		Memory.profilerDump [Game.time] = report;
+	}
+	
+	console.log ("Total used cpu: " + profiler.getUsedCpu ().toFixed (2) + " / " + Game.cpuLimit);
+	console.log ();
+}
+
 if (ENABLE_PROFILING) 
 {
     profiler.wrap (globals, 'require');	
 }
 
-var performRoles = require ('performRoles');
-var spawner = require ('spawner');
-var roomManager = require ('roomManager');
-var ProtoRole = require ('role_prototype');
-
-if (ENABLE_PROFILING)
-{
-	profiler.wrap (RoomPosition.prototype, 'findPathTo');
-    profiler.wrap (RoomPosition.prototype, 'findClosest');
-    
-    profiler.wrap (Game, 'getObjectById');
-	
-	profiler.wrap (roomManager, 'updateNeeds');
-	
-	profiler.wrap (ProtoRole.prototype, 'reset');
-}
-
-for (var i in Game.rooms)
-{
-	roomManager.reset ();
-	roomManager.setRoom (Game.rooms [i]);
-	
-	roomManager.initMemory ();
-	roomManager.updateNeeds ();
-}
-
-for (var i in Game.spawns)
-{
-	spawner (Game.spawns [i]);
-}
-
-performRoles (Game.creeps);
-
-if (Game.time % PROFILER_REPORT_INTERVAL == 0)
-{
-	Memory.profilerDump = Memory.profilerDump || {};
-	
-	var report = profiler.report ();
-	
-	profiler.logReport (report);
-	Memory.profilerDump [Game.time] = report;
-}
-
-console.log ("Total used cpu: " + profiler.getUsedCpu ().toFixed (2) + " / " + Game.cpuLimit);
-console.log ();
-
+act ();
